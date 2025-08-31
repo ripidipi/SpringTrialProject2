78e32d5a60614632fe7823fdbf5ae8d9a247ff9b
Ivan <ripidipin@gmail.com>
2025-08-31 17:29:18 +0300

    a bit dif usage of param atributs

--- src/main/java/app/trialProject/controllers/TestController.java
+++ src/main/java/app/trialProject/controllers/TestController.java
@@ -4,15 +4,15 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 public class TestController {
 
     @GetMapping("/hello")
-    public String helloPage(HttpServletRequest req) {
-        String name = req.getParameter("name");
-        String surname = req.getParameter("surname");
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname) {
 
         System.out.println("hello " + name + ' ' + surname);
 
